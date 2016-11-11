3d357b36ebb91d133aa07929dbb188ca8e81a8b8
leeym008 <leeym008@naver.com>
2016-11-11 15:26:38 +0900

    버튼2 누를시 이름 textview가 이름이 되도록 바꿈

--- app/src/main/java/com/example/a403/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -4,21 +4,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView t1;
     Button b1;
+    Button b2;
+    CalendarView c1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        t1 = (TextView)findViewById(R.id.textview);
+        c1 = (CalendarView)findViewById(R.id.calendarView);
         b1 = (Button)findViewById(R.id.button);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "안녕하세요", Toast.LENGTH_SHORT).show();
+            }
+        });
+        b2 = (Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t1.setText("이상원");
             }
         });
     }
