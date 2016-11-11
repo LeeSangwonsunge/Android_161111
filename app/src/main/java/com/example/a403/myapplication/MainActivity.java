0f33e5a1abe49e3b29fcd8c3a1b3763efbf03298
leeym008 <leeym008@naver.com>
2016-11-11 15:51:07 +0900

    checkbox 선택시 눌름 메시지 띄움 해제시 안눌름 메시지 띄움

--- app/src/main/java/com/example/a403/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     Button b1;
     Button b2;
     CalendarView c1;
+    CheckBox ch1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         t1 = (TextView)findViewById(R.id.textview);
         c1 = (CalendarView)findViewById(R.id.calendarView);
+        ch1 = (CheckBox)findViewById(R.id.checkBox);
         b1 = (Button)findViewById(R.id.button);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +44,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "존잘", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    Toast.makeText(getApplicationContext(), "눌림", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "안눌림", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
